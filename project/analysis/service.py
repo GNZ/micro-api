218740773db54a37216569a11dd0bda7992c416a
nicogarcia <nicoalbo90@gmail.com>
2015-12-25 16:11:39 -0300

    First time Watershed working

--- project/analysis/service.py
+++ project/analysis/service.py
@@ -3,54 +3,68 @@ import numpy
 from matplotlib import pyplot as plt
 from skimage import morphology
 from skimage import exposure
+from skimage.color import label2rgb
+from skimage.filters import rank
 from skimage.filters import sobel
+from scipy import ndimage as ndi
+from skimage.morphology import disk
 
 
 class ImageAnalysis:
     def analyse(self, image_id):
         image = cv2.imread('../../images/input/blood_100x.jpg')
 
+        numpy.set_printoptions(threshold='nan')
+
         if image is None:
             print('File not found')
             return
 
-        image = cv2.medianBlur(image, 5)
+        # image = cv2.medianBlur(image, 5)
 
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
         kernel = numpy.ones((5, 5), numpy.uint8)
-        gray = cv2.erode(gray, kernel)
-        gray = cv2.dilate(gray, kernel)
+        # gray = cv2.erode(gray, kernel)
+        #gray = cv2.dilate(gray, kernel)
 
-        # Remove black border
-        # _, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)
-        # removed_black = numpy.bitwise_or(gray, thresh)
+        gray = exposure.equalize_adapthist(gray)
 
-        # Adaptive thresh
-        gray_res = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 3)
+        for i in range(0, len(gray)):
+            for j in range(0, len(gray[0])):
+                gray[i, j] *= 255
 
-        # gray = exposure.equalize_hist(gray)
-        cv2.imwrite('gray.jpg', gray)
+        # output = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+        #cv2.imwrite('gray.jpg', output)
 
         elevation_map = sobel(gray)
+        # elevation_map = ndi.distance_transform_cdt(gray)
+
+        gray = gray.astype(int)
+
+        #print gray
 
         markers = numpy.zeros_like(gray)
-        markers[gray < 30] = 128
-        markers[gray > 150] = 255
+        markers[gray < 80] = 2  # seen as white in plot
+        markers[gray > 200] = 1  # seen as black in plot
+
+        #print markers
 
         segmentation = morphology.watershed(elevation_map, markers)
 
-        markers_dil = cv2.dilate(markers, kernel)
+        print segmentation
 
-        # Work over HSV
+        segmentation = ndi.binary_fill_holes(segmentation - 1)
 
-        image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-        image_h = cv2.extractChannel(image_hsv, 2)
+        labeled_coins, _ = ndi.label(segmentation)
+        image_label_overlay = label2rgb(labeled_coins, image=image)
+
+        print 'labeled coins '
+        #print labeled_coins
 
         images = [
             ['Grayscale', gray],
             ['Elevation', elevation_map],
-            ['Gray thres', gray_res],
             ['Markers', markers],
             ['Watershed seg', segmentation]
         ]
@@ -60,7 +74,9 @@ class ImageAnalysis:
             plt.subplot(2, 3, i + 1), plt.imshow(images[i][1], 'gray')
             plt.title(images[i][0])
             plt.xticks([]), plt.yticks([])
-            cv2.imwrite(images[i][0] + '.jpg', images[i][1])
+            # cv2.imwrite(images[i][0] + '.jpg', images[i][1])
+
+        plt.subplot(2, 3, 5), plt.imshow(image_label_overlay)
 
         plt.show()
 
